03b152a5e6d83d33244e7741bc656b7582008800
Bill Monkman <bill.monkman@hootsuite.com>
2017-01-04 10:12:00 -0800

    Bumped version

--- php_protobuf.h
+++ php_protobuf.h
@@ -5,7 +5,7 @@
 #include "config.h"
 #endif
 
-#define PHP_PROTOBUF_VERSION "0.11.0"
+#define PHP_PROTOBUF_VERSION "0.11.0.hoot"
 #define PHP_PROTOBUF_EXTNAME "protobuf"
 
 extern zend_module_entry protobuf_module_entry;
